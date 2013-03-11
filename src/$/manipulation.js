1c302317f6d8b394ea27603f0c5b67a66471b292
Niek Saarberg <n.saarberg@gmail.com>
2013-03-11 23:14:37 +0100

    append and appendTo now apply to whole collection

--- src/$/manipulation.js
+++ src/$/manipulation.js
@@ -2,18 +2,28 @@ PB.overwrite($.prototype, {
 	
 	append: function ( target ) {
 
+		var i = 0;
+
 		target = PB.$(target);
 
-		this[0].appendChild(target[0]);
+		for( ; i < target.length; i++ ) {
+
+			this[0].appendChild(target[i]);
+		}
 
 		return this;
 	},
 
 	appendTo: function ( target ) {
 
+		var i = 0;
+
 		target = PB.$(target);
 
-		target[0].appendChild(this[0]);
+		for( ; i < this.length; i++ ) {
+
+			target[0].appendChild(this[i]);
+		}
 
 		return this;
 	},
