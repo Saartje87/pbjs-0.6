9f7cb1b63f936ca64fdcfe6be27922e1beb96baa
Niek Saarberg <n.saarberg@gmail.com>
2013-03-19 20:34:21 +0100

    Comments..

--- src/$/buildfragment.js
+++ src/$/buildfragment.js
@@ -1,10 +1,19 @@
+/**
+ * Convert string to html nodes
+ *
+ * @param {String} html
+ * @return {Object} PB.$
+ */
 PB.$.buildFragment = function ( html ) {
 	
 	var fragment = doc.createElement('div'),
 		children;
 
+	// Table elements should be created in an table
+	// html: '<td></td>' wrap with:
 	fragment.innerHTML = html;
 
+	// Return the childeren
 	children = PB.$(fragment).children();
 
 	fragment = null;
