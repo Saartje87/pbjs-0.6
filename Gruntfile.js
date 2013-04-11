4e809c176009e51d75b9e2c77dca5e55b30b7e5e
Niek Saarberg <n.saarberg@gmail.com>
2013-04-11 22:26:44 +0200

    Using Grunt 0.4

---
+++ Gruntfile.js
@@ -0,0 +1,95 @@
+module.exports = function(grunt) {
+
+	var banner = 
+				'/*!\n' +
+				' * pbjs JavaScript Framework v<%= pkg.version %>\n' +
+				' * http://saartje87.github.com/pbjs\n' +
+				' *\n' +
+				' * Includes Qwery\n' +
+				' * https://github.com/ded/qwery\n' +
+				' *\n' +
+				' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>\n' +
+		        ' * Licensed <%= pkg.license %>\n' +
+				' *\n' +
+				' * Build date <%= grunt.template.today("yyyy-mm-dd HH:MM") %>\n' +
+				' */\n';
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		// Concat
+		concat: {
+			options: {
+
+				banner: banner
+			},
+			dist: {
+
+				src: ['<banner>', 'src/intro.js',
+
+					// Core
+					'src/core/utils.js', 'src/core/class.js',
+
+					// Patterns
+					'src/patterns/observer.js',
+					'src/patterns/queue.js',
+
+					// $ DOM
+					'src/dom/core.js', 'src/dom/utils.js', 'src/dom/hooks.js',
+					'src/dom/style.js', 'src/dom/morph.js', 'src/dom/manipulation.js',
+					'src/dom/layout.js',
+					'src/dom/stuff.js',
+					'src/dom/traversal.js',
+					'src/dom/event.js',
+					'src/dom/buildfragment.js',
+					'src/dom/selector.js',
+					'src/dom/enumerable.js',
+					'src/dom/animation.js',
+
+					'src/dom/legacy.js',
+
+					// Request
+					'src/request/request.js', 'src/request/utils.js',
+
+					// JSON
+
+					// String
+
+					// Outro
+					'src/outro.js'
+				],
+				dest: 'dist/pbjs.js'
+			}
+		},
+		jshint: {
+
+			options: {
+                jshintrc: '.jshintrc'
+            },
+            afterconcat: ['dist/pbjs.js']
+		},
+		// Uglify
+		uglify: {
+
+			options: {
+
+				report: 'gzip',
+				preserveComments: 'some'
+			},
+			build: {
+
+				src: ["dist/pbjs.js", /*"vendor/qwery/qwery.js"*/],
+				dest: "dist/pbjs.min.js"
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+
+	// Default task(s).
+	grunt.registerTask('default', ['concat', 'uglify']);
+	grunt.registerTask('test', ['jshint']);
+
+};
