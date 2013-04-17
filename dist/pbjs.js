9f280741162e72e147a2894b5baae998d163e819
Niek Saarberg <n.saarberg@gmail.com>
2013-04-17 17:56:42 +0200

    Source push

--- dist/pbjs.js
+++ dist/pbjs.js
@@ -8,7 +8,7 @@
  * Copyright 2013 Niek Saarberg
  * Licensed MIT
  *
- * Build date 2013-04-16 13:43
+ * Build date 2013-04-17 17:54
  */
 (function ( name, context, definition ) {
 	
@@ -658,6 +658,150 @@ PB.$.hook = function ( name, fn ) {
 	PB.$.hooks[name] = fn;
 };
 
+PB.overwrite(PB.$.fn, {
+
+	/**
+	 * Set the given attribute(s) for every element in de set.
+	 */
+	setAttr: function ( data ) {
+
+		var i = 0,
+			key;
+
+		data = argsToObject(arguments);
+
+		for( ; i < this.length; i++ ) {
+
+			for( key in data ) {
+
+				if( data.hasOwnProperty(key) ) {
+
+					this[i].setAttribute(key, data[key]);
+				}
+			}
+		}
+
+		return this;
+	},
+
+	/**
+	 * Get the attribute value from the first element in the set.
+	 */
+	getAttr: function ( key ) {
+
+		return this[0].getAttribute(key);
+	},
+
+	/**
+	 * Remove the given attribute(s) for every element in de set.
+	 */
+	removeAttr: function ( key ) {
+
+		var i = 0;
+
+		for( ; i < this.length; i++ ) {
+
+			this[i].removeAttribute(key);
+		}
+
+		return this;
+	},
+
+	/**
+	 * Set the given value for every element in the set.
+	 */
+	setValue: function ( value ) {
+
+		var i = 0;
+
+		for( ; i < this.length; i++ ) {
+
+			this[i].value = value;
+		}
+
+		return this;
+	},
+
+	/**
+	 * Get the value from the first element in the set.
+	 */
+	getValue: function () {
+
+		return this[0].value;
+	},
+
+	/**
+	 * Set data for every element in the set.
+	 */
+	setData: function ( data ) {
+
+		var i = 0,
+			storage;
+
+		data = argsToObject(arguments);
+
+		for( ; i < this.length; i++ ) {
+
+			storage = domGetStorage(this[i]);
+			storage.data = storage.data || {};
+
+			PB.overwrite(storage.data, data);
+		}
+
+		return this;
+	},
+
+	/**
+	 * Get data from first element in the set.
+	 *
+	 * @todo if key is not given, return all data? Merge memory data with data- attibute? 
+	 */
+	getData: function ( key ) {
+
+		// Read 'data-' attribute
+		var storage = domGetStorage(this[0]),
+			data;
+
+		// Read from memory if set
+		if( storage.data ) {
+
+			data = storage.data[key];
+		} 
+
+		// No data found yet, try from 'data-' attribute
+		if( data === undefined ) {
+
+			data = this[0].getAttribute('data-'+key);
+		}
+
+		return data;
+	},
+
+	/**
+	 * Remove data from every element in the set.
+	 */
+	removeData: function ( key ) {
+
+		var i = 0,
+			id;
+
+		for( ; i < this.length; i++ ) {
+
+			this[i].removeAttribute('data-'+key);
+
+			id = this[i].__PBID__;
+
+			if( !id || !PB.$.cache[id] ) {
+
+				continue;
+			}
+
+			delete PB.$.cache[id].data[key];
+		}
+
+		return this;
+	}
+});
 	// Used for tests
 var div = document.createElement('div'),
 	// Vendor prefixes
@@ -1129,421 +1273,6 @@ PB.overwrite(PB.$.fn, {
 		target.remove();
 
 		return this;
-	}
-});
-PB.overwrite(PB.$.fn, {
-
-	width: function () {
-
-		return this.getStyle('width', true);
-	},
-
-	innerWidth: function () {
-
-		return this.getStyle('width', true) + this.getStyle('paddingLeft', true) + this.getStyle('paddingRight', true);
-	},
-
-	outerWidth: function ( includeMargin ) {
-
-		var outerWidth = this.innerWidth() + this.getStyle('borderLeftWidth', true) + this.getStyle('borderRightWidth', true);
-
-		if( includeMargin ) {
-
-			outerWidth += this.getStyle('marginLeft', true) + this.getStyle('marginRight', true);
-		}
-
-		return outerWidth;
-	},
-
-	scrollWidth: function () {
-
-		return this[0].scrollWidth;
-	},
-
-	height: function () {
-
-		return this.getStyle('height', true);
-	},
-
-	innerHeight: function () {
-
-		return this.getStyle('height', true) + this.getStyle('paddingTop', true) + this.getStyle('paddingBottom', true);
-	},
-
-	outerHeight: function ( includeMargin ) {
-
-		var outerHeight = this.innerHeight() + this.getStyle('borderTopWidth', true) + this.getStyle('borderBottomWidth', true);
-
-		if( includeMargin ) {
-
-			outerHeight += this.getStyle('marginTop', true) + this.getStyle('marginBottom', true);
-		}
-
-		return outerHeight;
-	},
-
-	scrollHeight: function () {
-
-		return this[0].scrollHeight;
-	},
-
-	setScroll: function ( position ) {
-
-		var i = 0;
-
-		for( ; i < this.length; i++ ) {
-
-			if( position.top !== undefined ) {
-
-				this[i].scrollTop = position.top;
-			}
-
-			if( position.left !== undefined ) {
-
-				this[i].scrollLeft = position.left;
-			}
-		}
-
-		return this;
-	},
-
-	getScroll: function () {
-
-		return {
-
-			top: this[0].scrollTop,
-			left: this[0].scrollLeft
-		};
-	},
-
-	// position
-	position: function () {
-
-		var box = this[0].getBoundingClientRect();
-
-		return {
-
-			top: box.top + (window.scrollY || window.pageYOffset),
-			left: box.left + (window.scrollX || window.pageXOffset)
-		};
-	},
-
-	offset: function () {
-
-		var element = this[0],
-			box = {
-
-				top: 0,
-				left: 0
-			};
-
-		while( element ) {
-
-			box.top += element.offsetTop;
-			box.left += element.offsetLeft;
-
-			element = element.offsetParent;
-
-			if( !element || PB.$(element).getStyle('position') !== 'static' ) {
-
-				break;
-			}
-		}
-
-		return box;
-	}
-});
-PB.overwrite(PB.$.fn, {
-
-	each: function ( fn ) {
-
-		var _args = slice.call( arguments, 1 ),
-			i = 0;
-
-		for( ; i < this.length; i++ ) {
-
-			fn.apply(this[i], _args);
-		}
-
-		return this;
-	},
-
-	/**
-	 * Returns true if the first element in the set has the given class name.
-	 */
-	hasClass: function ( className ) {
-
-		return (' '+this.context.className+' ').indexOf(' '+className+' ') >= 0;
-	},
-
-	/**
-	 * Add class(es) to every element in the set.
-	 */
-	addClass: function ( classNames ) {
-
-		var i = 0,
-			classList = classNames.split(' '),
-			className,
-			j;
-
-		for( ; i < this.length; i++ ) {
-			
-			className = ' '+this[i].className+' ';
-
-			for( j = 0; j < classList.length; j++ ) {
-
-				// Skip if element already got the class
-				if( className.indexOf(' '+classList[j]+' ') >= 0 ) {
-				
-					continue;
-				}
-				
-				// Add class
-				this[i].className += (this[i].className ? ' ' : '')+classList[j];
-			}
-		}
-
-		return this;
-	},
-
-	/**
-	 * Removes class(es) from every element in the set.
-	 */
-	removeClass: function ( classNames ) {
-
-		var i = 0,
-			classList = classNames.split(' '),
-			l = classList.length,
-			className,
-			j;
-
-		for( ; i < this.length; i++ ) {
-
-			className = ' '+this[i].className+' ';
-
-			for( j = 0; j < l; j++ ) {
-				
-				// Already exists
-				if( className.indexOf(' '+classList[j]+' ') >= 0 ) {
-				
-					className = className.replace(' '+classList[j]+' ', ' ');
-				}
-			}
-
-			// Trim whitespaces
-			className = className.replace(/^\s|\s$/g, '');
-
-			// Update class list
-			if( className ) {
-
-				this[i].className = className;
-			}
-			// Remove class attribute
-			else {
-
-				this[i].removeAttribute('class');
-			}
-		}
-
-		return this;
-	},
-
-	/**
-	 * Set data for every element in the set.
-	 */
-	setData: function ( data ) {
-
-		var i = 0,
-			storage;
-
-		data = argsToObject(arguments);
-
-		for( ; i < this.length; i++ ) {
-
-			storage = domGetStorage(this[i]);
-			storage.data = storage.data || {};
-
-			PB.overwrite(storage.data, data);
-		}
-
-		return this;
-	},
-
-	/**
-	 * Get data from first element in the set.
-	 *
-	 * @todo if key is not given, return all data? Merge memory data with data- attibute? 
-	 */
-	getData: function ( key ) {
-
-		// Read 'data-' attribute
-		var storage = domGetStorage(this[0]),
-			data;
-
-		// Read from memory if set
-		if( storage.data ) {
-
-			data = storage.data[key];
-		} 
-
-		// No data found yet, try from 'data-' attribute
-		if( data === undefined ) {
-
-			data = this[0].getAttribute('data-'+key);
-		}
-
-		return data;
-	},
-
-	/**
-	 * Remove data from every element in the set.
-	 */
-	removeData: function ( key ) {
-
-		var i = 0,
-			id;
-
-		for( ; i < this.length; i++ ) {
-
-			this[i].removeAttribute('data-'+key);
-
-			id = this[i].__PBID__;
-
-			if( !id || !PB.$.cache[id] ) {
-
-				continue;
-			}
-
-			delete PB.$.cache[id].data[key];
-		}
-
-		return this;
-	},
-
-	/**
-	 * Set the given attribute(s) for every element in de set.
-	 */
-	setAttr: function ( data ) {
-
-		var i = 0,
-			key;
-
-		data = argsToObject(arguments);
-
-		for( ; i < this.length; i++ ) {
-
-			for( key in data ) {
-
-				if( data.hasOwnProperty(key) ) {
-
-					this[i].setAttribute(key, data[key]);
-				}
-			}
-		}
-
-		return this;
-	},
-
-	/**
-	 * Get the attribute value from the first element in the set.
-	 */
-	getAttr: function ( key ) {
-
-		return this[0].getAttribute(key);
-	},
-
-	/**
-	 * Remove the given attribute(s) for every element in de set.
-	 */
-	removeAttr: function ( key ) {
-
-		var i = 0;
-
-		for( ; i < this.length; i++ ) {
-
-			this[i].removeAttribute(key);
-		}
-
-		return this;
-	},
-
-	/**
-	 * Set the given value for every element in the set.
-	 */
-	setValue: function ( value ) {
-
-		var i = 0;
-
-		for( ; i < this.length; i++ ) {
-
-			this[i].value = value;
-		}
-
-		return this;
-	},
-
-	/**
-	 * Get the value from the first element in the set.
-	 */
-	getValue: function () {
-
-		return this[0].value;
-	},
-
-	/**
-	 * Shows every element in the set.
-	 */
-	show: function () {
-
-		var style,
-			i = 0;
-
-		for( ; i < this.length; i++ ) {
-
-			style = this[i].style;
-
-			if( style.display === 'none' ) {
-
-				style.display = domGetStorage(this[i])['css-display'] || 'block';
-			}
-		}
-
-		return this;
-	},
-
-	/**
-	 * Hides every element in the set.
-	 */
-	hide: function () {
-
-		var style,
-			i = 0;
-
-		for( ; i < this.length; i++ ) {
-
-			style = this[i].style;
-
-			if( style.display !== 'none' ) {
-
-				// Store css display value
-				domGetStorage(this[i])['css-display'] = PB.$(this[i]).getStyle('display');
-
-				// Hide element
-				style.display = 'none';
-			}
-		}
-
-		return this;
-	},
-
-	/**
-	 * Returns boolean whether the first element in the set is visible or not.
-	 */
-	isVisible: function () {
-
-		var element = PB.$(this[0]);
-
-		return element.getStyle('display') !== 'none' && element.getStyle('opacity') > 0;
 	},
 
 	/**
@@ -1683,7 +1412,264 @@ PB.overwrite(PB.$.fn, {
 		return this[0].textContent || this[0].nodeValue || '';
 	}
 });
+PB.overwrite(PB.$.fn, {
 
+	width: function () {
+
+		return this.getStyle('width', true);
+	},
+
+	innerWidth: function () {
+
+		return this.getStyle('width', true) + this.getStyle('paddingLeft', true) + this.getStyle('paddingRight', true);
+	},
+
+	outerWidth: function ( includeMargin ) {
+
+		var outerWidth = this.innerWidth() + this.getStyle('borderLeftWidth', true) + this.getStyle('borderRightWidth', true);
+
+		if( includeMargin ) {
+
+			outerWidth += this.getStyle('marginLeft', true) + this.getStyle('marginRight', true);
+		}
+
+		return outerWidth;
+	},
+
+	scrollWidth: function () {
+
+		return this[0].scrollWidth;
+	},
+
+	height: function () {
+
+		return this.getStyle('height', true);
+	},
+
+	innerHeight: function () {
+
+		return this.getStyle('height', true) + this.getStyle('paddingTop', true) + this.getStyle('paddingBottom', true);
+	},
+
+	outerHeight: function ( includeMargin ) {
+
+		var outerHeight = this.innerHeight() + this.getStyle('borderTopWidth', true) + this.getStyle('borderBottomWidth', true);
+
+		if( includeMargin ) {
+
+			outerHeight += this.getStyle('marginTop', true) + this.getStyle('marginBottom', true);
+		}
+
+		return outerHeight;
+	},
+
+	scrollHeight: function () {
+
+		return this[0].scrollHeight;
+	},
+
+	setScroll: function ( position ) {
+
+		var i = 0;
+
+		for( ; i < this.length; i++ ) {
+
+			if( position.top !== undefined ) {
+
+				this[i].scrollTop = position.top;
+			}
+
+			if( position.left !== undefined ) {
+
+				this[i].scrollLeft = position.left;
+			}
+		}
+
+		return this;
+	},
+
+	getScroll: function () {
+
+		return {
+
+			top: this[0].scrollTop,
+			left: this[0].scrollLeft
+		};
+	},
+
+	// position
+	position: function () {
+
+		var box = this[0].getBoundingClientRect();
+
+		return {
+
+			top: box.top + (window.scrollY || window.pageYOffset),
+			left: box.left + (window.scrollX || window.pageXOffset)
+		};
+	},
+
+	offset: function () {
+
+		var element = this[0],
+			box = {
+
+				top: 0,
+				left: 0
+			};
+
+		while( element ) {
+
+			box.top += element.offsetTop;
+			box.left += element.offsetLeft;
+
+			element = element.offsetParent;
+
+			if( !element || PB.$(element).getStyle('position') !== 'static' ) {
+
+				break;
+			}
+		}
+
+		return box;
+	},
+
+	/**
+	 * Returns true if the first element in the set has the given class name.
+	 */
+	hasClass: function ( className ) {
+
+		return (' '+this.context.className+' ').indexOf(' '+className+' ') >= 0;
+	},
+
+	/**
+	 * Add class(es) to every element in the set.
+	 */
+	addClass: function ( classNames ) {
+
+		var i = 0,
+			classList = classNames.split(' '),
+			className,
+			j;
+
+		for( ; i < this.length; i++ ) {
+			
+			className = ' '+this[i].className+' ';
+
+			for( j = 0; j < classList.length; j++ ) {
+
+				// Skip if element already got the class
+				if( className.indexOf(' '+classList[j]+' ') >= 0 ) {
+				
+					continue;
+				}
+				
+				// Add class
+				this[i].className += (this[i].className ? ' ' : '')+classList[j];
+			}
+		}
+
+		return this;
+	},
+
+	/**
+	 * Removes class(es) from every element in the set.
+	 */
+	removeClass: function ( classNames ) {
+
+		var i = 0,
+			classList = classNames.split(' '),
+			l = classList.length,
+			className,
+			j;
+
+		for( ; i < this.length; i++ ) {
+
+			className = ' '+this[i].className+' ';
+
+			for( j = 0; j < l; j++ ) {
+				
+				// Already exists
+				if( className.indexOf(' '+classList[j]+' ') >= 0 ) {
+				
+					className = className.replace(' '+classList[j]+' ', ' ');
+				}
+			}
+
+			// Trim whitespaces
+			className = className.replace(/^\s|\s$/g, '');
+
+			// Update class list
+			if( className ) {
+
+				this[i].className = className;
+			}
+			// Remove class attribute
+			else {
+
+				this[i].removeAttribute('class');
+			}
+		}
+
+		return this;
+	},
+
+	/**
+	 * Shows every element in the set.
+	 */
+	show: function () {
+
+		var style,
+			i = 0;
+
+		for( ; i < this.length; i++ ) {
+
+			style = this[i].style;
+
+			if( style.display === 'none' ) {
+
+				style.display = domGetStorage(this[i])['css-display'] || 'block';
+			}
+		}
+
+		return this;
+	},
+
+	/**
+	 * Hides every element in the set.
+	 */
+	hide: function () {
+
+		var style,
+			i = 0;
+
+		for( ; i < this.length; i++ ) {
+
+			style = this[i].style;
+
+			if( style.display !== 'none' ) {
+
+				// Store css display value
+				domGetStorage(this[i])['css-display'] = PB.$(this[i]).getStyle('display');
+
+				// Hide element
+				style.display = 'none';
+			}
+		}
+
+		return this;
+	},
+
+	/**
+	 * Returns boolean whether the first element in the set is visible or not.
+	 */
+	isVisible: function () {
+
+		var element = PB.$(this[0]);
+
+		return element.getStyle('display') !== 'none' && element.getStyle('opacity') > 0;
+	}
+});
 PB.overwrite(PB.$.fn, {
 
 	/**
@@ -1964,7 +1950,7 @@ Event.prototype = {
 				return false;
 			}
 
-		} while ( target = target.parentNode )
+		} while ( target = target.parentNode );
 
 		// No match
 		return false;
@@ -2329,34 +2315,50 @@ PB.$.buildFragment = function ( html ) {
 	return children;
 };
 
-/**
- *
- */
-function domFilter ( filter ) {
+PB.overwrite(PB.$.fn, {
 
-	var res = [],
-		i = 0;
+	/**
+	 *
+	 */
+	// should be forEach
+	each: function ( fn ) {
 
-	for( ; i < this.length; i++ ) {
+		var _args = slice.call( arguments, 1 ),
+			i = 0;
 
-		if( typeof filter === 'string' ) {
+		for( ; i < this.length; i++ ) {
 
-			if( PB.$.selector.matches(this[i], filter) ) {
+			fn.apply(this[i], _args);
+		}
+
+		return this;
+	},
+
+	/**
+	 *
+	 */
+	filter: function ( filter ) {
+
+		var res = [],
+			i = 0,
+			filterIsString = typeof filter === 'string';
+
+		for( ; i < this.length; i++ ) {
+
+			if( filterIsString ) {
+
+				if( PB.$.selector.matches(this[i], filter) ) {
+
+					res.push(this[i]);
+				}
+			} else if ( filter(this[i]) === true ) {
 
 				res.push(this[i]);
 			}
-		} else if ( filter.call(null, this[i]) === true ) {
-
-			res.push(this[i]);
 		}
+
+		return new this.constructor(res);
 	}
-
-	return new this.constructor(res);
-}
-
-PB.overwrite(PB.$.fn, {
-
-	filter: domFilter
 });
 
 // Animation handler
@@ -2734,7 +2736,7 @@ PB.$.selector = {
 			}
 		},
 
-		mouse_ie: {
+		mouseIe: {
 
 			matches: /(!?mouse|click|focus|drag)/,
 			hook: function ( event, originalEvent ) {
@@ -2764,7 +2766,7 @@ PB.$.selector = {
 		
 		this.defaultPrevented = true;
 		this.cancelBubble = true;
-	},
+	};
 	
 	/**
 	 * Cancels the event if it is cancelable, without stopping further propagation of the event.
@@ -2772,7 +2774,7 @@ PB.$.selector = {
 	PB.$.Event.prototype.preventDefault = function () {
 		
 		this.returnValue = false;
-	}
+	};
 
 	/**
 	 * Destroy element cache
