35a8423f30feb55c9d0c6502b921492f78803c0c
Niek Saarberg <n.saarberg@gmail.com>
2013-08-27 14:11:50 +0200

    #36 using transition hook

--- dist/pbjs-legacy.js
+++ dist/pbjs-legacy.js
@@ -8,7 +8,7 @@
  * Copyright 2013 Niek Saarberg
  * Licensed MIT
  *
- * Build date 2013-08-27 13:53
+ * Build date 2013-08-27 14:10
  */
 (function ( context ) {
 
@@ -402,99 +402,71 @@ if( 'currentStyle' in div && !window.getComputedStyle ) {
 // Create a fallback for the morph method if transition are not supported
 if( !('transition' in div.style) && !('MozTransition' in div.style) && !('WebkitTransition' in div.style) ) {
 
-	/**
-	 * Convert arguments to ordered object
-	 */
-	function morphArgsToObject ( args ) {
-
-		// Default options
-		var i = 1,
-			effect,
-			options = {
-				
-				duration: 0.4,
-				effect: 'ease'
-			};
-		
-		// Loop trough args
-		for( ; i < args.length; i++ ) {
-
-			switch( typeof args[i] ) {
-				
-				case 'function':
-					options.fn = args[i];
-					break;
-
-				case 'number':
-					options.duration = args[i];
-					break;
-			
-				case 'string':
-					// easeInOut -> ease-in-out
-					effect = args[i].replace(/([A-Z])/g, '-$1').toLowerCase();
-
-					if( /^linear|ease|ease-in|ease-out|ease-in-out|cubic-bezier\(.*?\)$/.test(effect) ) {
-
-						options.effect = effect;
-					}
-					break;
-			}
-		}
-		
-		return options;
-	}
-
-	PB.$.fn.transition = function ( properties ) {
-
-		// Normalize arguments
-		var options = morphArgsToObject( arguments );
+	PB.$.hook('transition', function ( options ) {
 
 		this.stop(false);
 
 		return this.each(function () {
 
 			var element = PB.$(this),
-				currentStyles = {},
-				styleValueDiff = {},
-				animation;
+				queue = element.getData('pbjs-fx-queue'),
+				properties = options.properties;
 
-			// Calculate current styles
-			PB.each(properties, function ( property ) {
-				
-				currentStyles[property] = element.getStyle( property, true );
-			});
+			if( !queue ) {
 
-			// Calculate the difference between the given and current styles
-			PB.each(properties, function ( property ) {
+				queue = new PB.Queue();
+				element.setData('pbjs-fx-queue', queue);
+			}
 
-				var value = properties[property];
+			queue.add(function ( next, data ) {
 
-				value = /^-?[\d.]+px$/i.test( value ) ? parseInt(value, 10) : value;
-				
-				styleValueDiff[property] = value - currentStyles[property];
-			});
+				var //element = PB.$(this),
+					currentStyles = {},
+					styleValueDiff = {},
+					animation;
 
-			animation = new PB.Animation({
+				// Calculate current styles
+				PB.each(properties, function ( property ) {
+					
+					currentStyles[property] = element.getStyle(property, true);
+				});
 
-				duration: options.duration,
-				effect: options.effect,
-				onTick: function ( pos ) {
+				// Calculate the difference between the given and current styles
+				PB.each(properties, function ( property ) {
 
-					PB.each(styleValueDiff, function ( style, value ) {
+					var value = properties[property];
 
-						element.setStyle(style, currentStyles[style]+(value*pos));
-					});
+					value = /^-?[\d.]+px$/i.test( value ) ? parseInt(value, 10) : value;
+					
+					styleValueDiff[property] = value - currentStyles[property];
+				});
 
-					if( pos === 1 && options.fn ) {
+				animation = new PB.Animation({
 
-						options.fn( element );
+					duration: options.duration,
+					effect: options.effect,
+					onTick: function ( pos ) {
+
+						PB.each(styleValueDiff, function ( style, value ) {
+
+							element.setStyle(style, currentStyles[style]+(value*pos));
+						});
+
+						if( pos === 1 ) {
+
+							options.fn && options.fn( element );
+
+							next();
+						}
 					}
-				}
-			}).start();
+				}).start();
 
-			element.setData('morph', animation);
+				element.setData('morph', animation);
+			});
+
+			queue.run();
 		});
-	};
+	});
 
 	PB.$.fn.stop = function ( gotoEnd ) {
 
