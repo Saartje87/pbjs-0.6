824208b8709d04ba43c2f9e55659ba2475d32806
Niek Saarberg <n.saarberg@gmail.com>
2013-10-03 16:27:26 +0200

    getStyle fixed no calcultions for undefined values

--- dist/pbjs-legacy.js
+++ dist/pbjs-legacy.js
@@ -369,7 +369,7 @@ if( !supportsGetComputedStyle ) {
 			// Calculate non pixel values
 
 			// Is not a pixel number
-			if( !rpixel.test(value) && !rnum.test(value) ) {
+			if( value && !rpixel.test(value) && !rnum.test(value) ) {
 
 				div = document.createElement('div');
 				div.style.cssText = 'visbility: hidden; position: absolute; line-height: 0;';
